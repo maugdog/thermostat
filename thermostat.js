ee0c3cbe0f46258b4077f1fcf0aabf6cced90fab
Max Montgelas <max@maugdog.com>
2015-12-30 23:24:47 -0500

    Refactor into package

--- thermostat.js
+++ thermostat.js
@@ -1,99 +1,54 @@
-var tools = require('./tools.js'); // Load misc tools
-var max31855 = require('max31855');
-var util = require('util');
-var clc = require('cli-color');
-
-// Read any command line arguments and establish default options
-var args = tools.processCLArgs({
-  mode: 'heat', // heat || cool.
-  units: 'f', // f for Fahrenheit, c for Celsius, k for Kelvin.
-  temp: 80, // Target temperature in specified units.
-  time: -1, // If greater than -1, then specifies the length of time (in ms) to hold the target temp.
-  tolerance: 2, // Allowable tolerance in degrees of specified units.
-  frequency: 1000, // Frequency of temperature samples in milliseconds.
-  buffer: 2000 // The number of milliseconds that the switch must remain OFF before it can be turned BACK on. Prevents rapid switching cycles that can damage electronics.
-});
-
-// Dummy function for testing
-function Dummy(options) {
-  this.thermostat = options.hasOwnProperty('thermostat') ? options.thermostat : null;
-  this.temp = options.start;
-  this.increment = options.increment;
-  this.decrement = options.decrement;
-
-  /** Return a random integer! */
-
-  this.readDummyTemp = function(callback) {
-    if(thermostat.isOn) {
-      this.temp += this.increment * (thermostat.isHeater ? 1 : -1);
-    } else if(thermostat._lastStateChange) {
-      this.temp -= this.decrement * (thermostat.isHeater ? 1 : -1);
-    }
-
-    if(callback) {
-      callback(Number((this.temp).toFixed(2)));
-    } else {
-      console.log('Error: Read request issued with no callback.');
-    }
-  };
-
-  this.readTemp = this.readDummyTemp;
-}
-
-function max31855Units(unitString) {
-  switch(unitString) {
-    case 'c':
-      return max31855.UNITS.CELSIUS;
-    case 'k':
-      return max31855.UNITS.KELVIN;
-    case 'f':
-    default:
-      return max31855.UNITS.FAHRENHEIT;
-  }
-}
-
 // See above args for options
 function Thermostat(options) {
-  this.isHeater = options.mode === 'heat';
-  this._units = options.units;
-  this.currentTemp = NaN;
-  this.targetTemp = parseFloat(options.temp);
-  this.holdTime = parseInt(options.time,10);
-  this.tolerance = parseFloat(options.tolerance);
-  this.frequency = parseInt(options.frequency,10);
-  this.buffer = parseInt(options.buffer,10);
-  this.isOn = false;
+  this.isHeater = options.hasOwnProperty('isHeater') ? options.isHeater : true;
+  this.targetTemp = options.hasOwnProperty('targetTemp') ? options.targetTemp : 80;
+  this.holdTime = options.hasOwnProperty('holdTime') ? options.holdTime : -1;
+  this.tolerance = options.hasOwnProperty('tolerance') ? options.tolerance : 2;
+  this.frequency = options.hasOwnProperty('frequency') ? options.frequency : 1000;
+  this.buffer = options.hasOwnProperty('buffer') ? options.buffer : 2000;
 
+  this.currentTemp = NaN;
+
+  /** The thermoSensor object must define a readTemp(callback) method that passes the current
+    temperature value as a float (in degrees celsius) to the callback.
+  */
+  this.thermoSensor = null;
+
+  /** The powerswitch object must define an isOn property, as well as setOn() and setOff() methods. */
+  this.powerswitch = null;
+
+  /** If defined, this handler will be called on each read of the thermoSensor.
+    It is passed the calling ThermoStat instance as its only param. */
+  this.afterTempRead = null;
+
+  // Private
+  // The setInterval object used as a timer for sensor probing
   this._interval = null;
-  //this._thermoSensor = new max31855.ThermoSensor({units: max31855Units(this._units)}); // Load the MAX31855 thermocouple amplifier library
-  this._thermoSensor = new Dummy({start: 40, increment: .5, decrement: .2, thermostat: this});
   this._isReading = false;
   this._timeTargetTempReached = 0; // The time at which target temp was first reached
   this._lastStateChange = 0;
   this._allowTolerance = false;
 }
 
-// Set the units for the thermostat
-Thermostat.prototype._setUnits = function(units) {
-  this._units = units;
-  this._thermoSensor.units = max31855Units(this._units);
-}
-
-Thermostat.prototype._flipSwitch = function(shouldBeOn) {
-  // Flip the relay switch here!
+// Updates the thermostat's options
+Thermostat.prototype.updateOptions = function(options) {
+  if(options.hasOwnProperty('isHeater')) { this.isHeater = options.isHeater; }
+  if(options.hasOwnProperty('targetTemp')) { this.targetTemp = options.targetTemp; }
+  if(options.hasOwnProperty('holdTime')) { this.holdTime = options.holdTime; }
+  if(options.hasOwnProperty('tolerance')) { this.tolerance = options.tolerance; }
+  if(options.hasOwnProperty('frequency')) { this.frequency = options.frequency; }
+  if(options.hasOwnProperty('buffer')) { this.buffer = options.buffer; }
 }
 
 // Set the state to ON
 Thermostat.prototype._setState = function(shouldBeOn) {
   var currentTime = new Date().getTime();
-  if(!this.isOn && shouldBeOn) {
+  if(!this.powerswitch.isOn && shouldBeOn) {
     this._lastStateChange = currentTime;
-    this.isOn = true;
-    this._flipSwitch(true);
-  } else if(this.isOn && !shouldBeOn) {
+    this.powerswitch.setOn();
+  } else if(this.powerswitch.isOn && !shouldBeOn) {
     this._lastStateChange = currentTime;
-    this.isOn = false;
-    this._flipSwitch(false);
+    this.powerswitch.setOff();
   }
 };
 
@@ -120,7 +75,7 @@ Thermostat.prototype._setStateForTemp = function(tempReading) {
     }
   }
 
-  // If just reaching target temp for the first time, the set the reach time
+  // If just reaching target temp for the first time, then set _timeTargetTempReached
   if(this.holdTime >= 0 && !this._timeTargetTempReached
       && ((this.isHeater && tempReading >= this.targetTemp)
       || (!this.isHeater && tempReading <= this.targetTemp))) {
@@ -128,28 +83,49 @@ Thermostat.prototype._setStateForTemp = function(tempReading) {
   }
 };
 
-// Start checking the thermocouple reading on regular intervals
-Thermostat.prototype.run = function() {
-  var self = this; // Scope closure
-  this._interval = setInterval(function () {
-    // Make sure that the hold time hasn't been reached
+// Returns the time remaining in milliseconds
+Thermostat.prototype.timeRemaining = function() {
+  var timeRemaining = this.holdTime;
+  if(this.holdTime >= 0 && this._timeTargetTempReached) {
     var currentTime = new Date().getTime();
-    if(self.holdTime >= 0 && self._timeTargetTempReached && currentTime - self._timeTargetTempReached >= self.holdTime) {
-      self.stop();
+    timeRemaining = Math.max(0, this.holdTime - (currentTime - this._timeTargetTempReached));
+  }
+  return timeRemaining;
+}
+
+// Start checking the thermoSensor reading on regular intervals
+Thermostat.prototype.run = function() {
+  if(this.thermoSensor) {
+    if(!this.powerswitch) {
+      console.error('Thermostat warning: No switch object provided. Will run as a thermometer until you set the powerswitch property.');
     }
 
-    if(!self._isReading) {
-      self._isReading = true;
-      self._thermoSensor.readTemp(function(temp) {
-        self.currentTemp = temp;
-        self._isReading = false;
-        self._setStateForTemp(temp);
-        self._outputStateToCLI();
-      });
-    } else {
-      console.error('Error: Still waiting for temperature from SPI bus. Perhaps your read frequency is too high, or your MAX31855 board is not connected properly?');
-    }
-  }, args.frequency);
+    // Good to go, start running this thing!
+    var self = this; // Scope closure
+    this._interval = setInterval(function () {
+      // Make sure that the hold time hasn't been reached
+      var currentTime = new Date().getTime();
+      if(self.holdTime >= 0 && self._timeTargetTempReached && currentTime - self._timeTargetTempReached >= self.holdTime) {
+        self.stop();
+      }
+
+      if(!self._isReading) {
+        self._isReading = true;
+        self.thermoSensor.readTemp(function(temp) {
+          self.currentTemp = temp;
+          self._isReading = false;
+          if(self.powerswitch) { self._setStateForTemp(temp); }
+
+          // Call the afterTempRead handler
+          if(self.afterTempRead) {self.afterTempRead(self);}
+        });
+      } else {
+        console.error('Thermostat error: Still waiting for temperature from thermoSensor. Perhaps your read frequency is too high, or your sensor is not connected properly.');
+      }
+    }, this.frequency);
+  } else {
+    console.error('Thermostat error: No sensor object provided. Please set the thermoSensor property.');
+  }
 };
 
 // Stops the thermostat, setting the switch off, and canceling the temperature check intervals
@@ -161,54 +137,4 @@ Thermostat.prototype.stop = function() {
   this._setState(false);
 };
 
-Thermostat.prototype._timeRemainingLabel = function() {
-  if(this.holdTime >= 0) {
-    var timeRemaining = this.holdTime;
-    if(this._timeTargetTempReached) {
-      var currentTime = new Date().getTime();
-      timeRemaining = Math.floor(Math.max(0, this.holdTime - (currentTime - this._timeTargetTempReached))/1000);
-    } else {
-      Math.floor(timeRemaining /= 1000);
-    }
-
-    var hours   = Math.floor(timeRemaining / 3600);
-    var minutes = Math.floor((timeRemaining - (hours * 3600)) / 60);
-    var seconds = timeRemaining - (hours * 3600) - (minutes * 60);
-
-    // Pad single digit nums
-    if (hours   < 10) {hours   = '0' + hours;}
-    if (minutes < 10) {minutes = '0' + minutes;}
-    if (seconds < 10) {seconds = '0' + seconds;}
-
-    return util.format('Remaining: %s:%s:%s\t\t', hours, minutes, seconds);
-  }
-  return '';
-}
-
-Thermostat.prototype._outputStateToCLI = function() {
-  process.stdout.write(clc.erase.screen);
-  process.stdout.write(clc.move.to(0, 0));
-  var stateText = null;
-  var stateLabel = this.isHeater ? "Heater" : "Chiller";
-  if(this.isHeater) {
-    stateText = this.isOn ? clc.xterm(202)('ON') : clc.green('OFF');
-  } else {
-    stateText = this.isOn ? clc.blue('ON') : clc.green('OFF');
-  }
-  process.stdout.write(util.format('%sTemp(°%s): %s\t\t%s: %s\n\n', this._timeRemainingLabel(), this._units, this.currentTemp, stateLabel, clc.bold(stateText)));
-}
-
-var thermostat = new Thermostat(args);
-thermostat.run();
-
-var exitHandler = function(options, err) {
-  thermostat.stop();
-
-  if (options.cleanup) console.log('clean');
-  if (err) console.log(err.stack);
-  if (options.exit) process.exit();
-};
-
-process.on('exit', exitHandler.bind(null,{cleanup:true})); // Handle app close events
-process.on('SIGINT', exitHandler.bind(null, {exit:true})); // Catches ctrl+c event
-process.on('uncaughtException', exitHandler.bind(null, {exit:true})); // Catches uncaught exceptions
+module.exports = Thermostat;
